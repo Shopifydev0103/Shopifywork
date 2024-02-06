37e68d7a2a7fe4ed53892595075960c3e410ebf2
dragon <dragondev0103@gmail.com>
2024-02-06 12:05:17 -0800

    fixed responsive swiper-slider.liquid and header part

--- assets/swiper.js
+++ assets/swiper.js
@@ -32,4 +32,21 @@
     slidesOffsetBefore: 0,
     //
     grabCursor: true,
+    breakpoints: {
+      // when window width is >= 320px
+      425: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    }
   })        
